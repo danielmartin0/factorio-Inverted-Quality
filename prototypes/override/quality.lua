1d099ea3f23543dd4338fb1e84c630b49870317f
danielmartin0 <github@danielgmartin.com>
2025-05-05 20:31:20 +0100

    see changelog.txt

--- prototypes/override/quality.lua
+++ prototypes/override/quality.lua
@@ -1,7 +1,4 @@
 data.raw.quality.normal.color = { 238, 238, 238 }
-data.raw.quality.normal.beacon_power_usage_multiplier = 1
-data.raw.quality.normal.mining_drill_resource_drain_multiplier = 1
-data.raw.quality.normal.science_pack_drain_multiplier = 1
 data.raw.quality.normal.next = "shoddy"
 
 for name, quality in pairs(data.raw.quality) do
